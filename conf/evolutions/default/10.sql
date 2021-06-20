c96c43524c507753c6d7eaa2358f059c4281cbdf
Рязанов Леонид <ryazanov@tochkak.ru>
2021-06-20 03:13:34 +0300

    Add app secret env

---
+++ conf/evolutions/default/10.sql
@@ -0,0 +1,5 @@
+# --- !Ups
+
+insert into users (first_name, last_name, email, password, phone, telegram, created, updated, deleted) values('Admin', 'Test', 'admin@test.ru', 'd8578edf8458ce06fbc5bb76a58c5ca4', '89001001010', null, '2021-06-20 00:00:00', '2021-06-20 00:00:00', null);
+
+# --- !Downs
--- conf/evolutions/default/9.sql
+++ conf/evolutions/default/9.sql
@@ -1,9 +1,9 @@
 # --- !Ups
 
-UPDATE priority SET color =  '#dc3545' WHERE id = 1;
-UPDATE priority SET color =  '#ffc107' WHERE id = 2;
-UPDATE priority SET color = '##198754' WHERE id = 3;
-UPDATE priority SET color =  '#0d6efd' WHERE id = 4;
+UPDATE priority SET color = '#dc3545' WHERE id = 1;
+UPDATE priority SET color = '#ffc107' WHERE id = 2;
+UPDATE priority SET color = '#198754' WHERE id = 3;
+UPDATE priority SET color = '#0d6efd' WHERE id = 4;
 
 # --- !Downs
 UPDATE priority SET color = '#A31012' WHERE id = 1;
