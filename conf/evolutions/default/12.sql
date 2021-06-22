3bdfbf6524884b652af47bab51b6c945aa5d9b88
Рязанов Леонид <ryazanov@tochkak.ru>
2021-06-22 22:31:19 +0300

    Fix telegram

--- conf/evolutions/default/12.sql
+++ conf/evolutions/default/12.sql
@@ -1,9 +1,7 @@
 # --- !Ups
 
 alter table task add column is_notified boolean not null default false;
-alter table users drop column telegram;
 
 # --- !Downs
 
 alter table task drop column is_notified;
-alter table users add column telegram varchar null;
